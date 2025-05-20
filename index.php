f14c6cc816c0afda6e466ea789a35313a8a60ff2
sara <sramos154q@fpdrioja.com>
2025-05-20 12:20:10 +0000

    documentacion incluida

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
+/**
+*Procesamiento de correo electronico válido
+*Recibe un correo electronico a través de POST 
+*Si es correcto lo válida y da mensaje
+*Si no es correcto da un mensaje avisandolo. 
+*
+*@package ValidacionEmail
+*@autor Sara Ramos
+*@version 1.0
+*
+**/
+
 function procesarEmailFormulario(){
 
+/**
+*Email a validar
+*@var string  $email
+**/
     $email =$_POST['email'] ?? '';
+
+/**
+*Condicional que valida 
+*@var string @email
+**/
     if($email){
         echo "Email '". htmlspecialchars($email) . "' recibido con éxito.";
     }else {
